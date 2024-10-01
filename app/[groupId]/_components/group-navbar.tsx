1b144a00019195d69ff11bdf8680b938d5965c2e
Eveyoru <107825906+Eveyoru@users.noreply.github.com>
2024-10-01 11:23:11 +0545

    Update group-navbar.tsx

--- app/[groupId]/_components/group-navbar.tsx
+++ app/[groupId]/_components/group-navbar.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Button } from "@/components/ui/button"
-import { Id } from "@/convex/_generated/dataModel";
+//import { Id } from "@/convex/_generated/dataModel";
 import { useParams, useRouter } from "next/navigation";
 
 export const GroupNavbar = () => {
